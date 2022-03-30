3ea35c3bd5aeb167f837fdc7d657957bf4b63c42
kinyarasam <samkinyara2000@gmail.com>
2022-03-30 19:23:40 +0300

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef main_H
 #define main_H
+
 extern char *_strcat(char *dest, char *src); 
 extern char *_strncat(char *dest, char *src, int n);
 extern char *_strncpy(char *dest, char *src, int n);
@@ -8,10 +9,11 @@ extern char *string_toupper(char *);
 extern char *cap_string(char *);
 extern char *leet(char *);
 extern char *rot13(char *);
-extern void print_number(int n):
+extern void print_number(int n);
 extern int _putchar(char c);
 extern char *infinite_add(char *n1, char *n2, char *r, int size_r);
 extern void print_buffer(char *b, int size);
 void print_buffer(char *b, int size);
 extern int _strcmp(char *s1, char *s2);
+
 #endif

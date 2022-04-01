0e9792eaecc56e44be80d5d59f48dd489e2d0449
kinyarasam <samkinyara2000@gmail.com>
2022-04-01 23:05:53 +0300

    push

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - check for upper_case letter
+ * @c: character to check the case
+ * Return: Always 0. (Success)
+ * otherwise: Always 1.
+ */
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}

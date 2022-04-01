9313e56a96c84b0caf35150c626b1f423b35f459
kinyarasam <samkinyara2000@gmail.com>
2022-04-01 13:22:21 +0300

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,13 +12,13 @@ int main(int argc, char *argv[])
 {
 	int i, mul = 1;
 	
-	if(argc != 3)
+	if(argc == 3)
 	{
-		printf("Error\n");
-		return (1);
+		for(i = 1; i < argc; i++)
+			mul *= atoi(argv[i]);
+		return (0);
 	}
-	for(i = 1; i < argc; i++)
-		mul *= atoi(argv[i]);
+	printf("Error\n");
+	return (1);
 	printf("%d\n", mul);
-	return (0);
 }

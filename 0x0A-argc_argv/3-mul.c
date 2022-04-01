0ed0f80cd6d5875aebc58d1569b905e916e66b17
kinyarasam <samkinyara2000@gmail.com>
2022-04-01 13:35:41 +0300

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * main - print the result of the multiplication, followed by a new line
- * @argc: int
- * @argv: list
- * Return: 0
- */
 
+/**
+ * main - Multiplies two numbers
+ * @argc: arguemenet count
+ * @argv: arguement vector
+ * Return: Always 1 (Fail)
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char const *argv[])
 {
-(void)argc;
+	(void)argc;
 
-if (argc != 3)
-{
-	printf("Error\n");
-	return (1);
-}
-printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
-return (0);
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }

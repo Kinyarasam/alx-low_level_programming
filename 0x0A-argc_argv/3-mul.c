5b4573e13f02a0a9f849ec2a202c96e86f89bdda
kinyarasam <samkinyara2000@gmail.com>
2022-04-01 13:29:24 +0300

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,15 +8,14 @@
  * Return: Always 1 (Fail)
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
 {
-	(void)argc;
+(void)argc;
 
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
-	return (0);
+if (argc != 3)
+{
+	printf("Error\n");
+	return (1);
+}
+printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+return (0);
 }

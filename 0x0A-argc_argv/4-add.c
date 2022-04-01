0746a18fd3de20b795d794433fe680e415b12108
kinyarasam <samkinyara2000@gmail.com>
2022-04-01 13:44:23 +0300

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,41 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * isInteger - checks if s is an integer
+ * @s: string to check
+ * Return: 0 or 1
+ */
+
+int isInteger(const char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+	if (s[i] < '0' || s[i] > '9')
+		return (1);
+	i++;
+}
+return (0);
+}
 
 /**
- * isInt - Checks if s is an interger
- * @s: string to check.
- * Return: Always 0.(success)
- * Return: Always 1.(Error)
+ * main - adds positive numbers
+ * @argc: int
+ * @argv: list
+ * Return: 0
  */
-int isInt(char *s)
+
+int main(int argc, char const *argv[])
 {
-	int i = 0;
-	while (s[i] != '\0')
+int sum = 0;
+while (--argc)
+{
+	if (isInteger(argv[argc]))
 	{
-		if (s[i] < '0' || s[i] > '9')
-			return (1);
-		i++;
+		printf("Error\n");
+		return (1);
 	}
-	return (0);
+	sum += atoi(argv[argc]);
 }
-/**
- * main - adds the positive numbers
- * @argc: arguement count
- * @argv: arguement vector
- * Return: Always 0.
- */
-int main(int argc, char *argv[])
-{
-	int sum = 0;
-	while (--argc)
-	{
-		if (isInt(argv[argc]))
-		{
-			printf("Error\n");
-			return (1);
-		}
-		sum += atoi(argv[argc]);
-	}
-	printf("%i\n", sum);
-	return (0);
+
+printf("%i\n", sum);
+
+return (0);
 }

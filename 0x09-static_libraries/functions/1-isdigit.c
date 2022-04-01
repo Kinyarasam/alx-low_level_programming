964983f1e3f6e5fe549c3b2f4085c39cab557e90
kinyarasam <samkinyara2000@gmail.com>
2022-04-01 21:47:43 +0300

    1-isdigit.c

---
+++ 0x09-static_libraries/functions/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isdigit - check for a digit
+ * @c: character to check
+ * Return: Always 0. (Success) 
+ * otherwise 1.
+ */
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+
+}

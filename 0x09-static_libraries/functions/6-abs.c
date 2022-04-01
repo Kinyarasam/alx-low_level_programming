4e4fe44ef858d478479679e4b8833f84beca4578
kinyarasam <samkinyara2000@gmail.com>
2022-04-01 22:16:50 +0300

    6-abs.c

---
+++ 0x09-static_libraries/functions/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - return abs value
+ * @n : number to check
+ * Return: Always 0. (Success) 
+ * otherwise 1.
+ */
+int _abs(int n)
+{
+	return (n >= 0 ? n : n * -1);
+}

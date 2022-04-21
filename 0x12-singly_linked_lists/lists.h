b0c76fcbe58c0f62d4bd2bb1537be096de5eac0c
kinyarasam <samkinyara2000@gmail.com>
2022-04-21 22:17:19 +0300

    lists.h

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,24 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * struct list_s - stingly linked list
+ * @str: malloc'ed string 
+ * @len: length of the string
+ * @next: next node.
+ *
+ * Description - Stingly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif

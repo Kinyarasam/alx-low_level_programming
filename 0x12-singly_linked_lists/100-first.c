ae5860e06f1a5cffca2803af72497507c7911a85
kinyarasam <samkinyara2000@gmail.com>
2022-04-22 22:15:16 +0300

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * func - executes program before the main
+ *
+ * Return: nothing
+ */
+void func(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}

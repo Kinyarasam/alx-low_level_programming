1c020d01729bf02a3496939872599b6cc44306a5
kinyarasam <samkinyara2000@gmail.com>
2022-04-22 22:01:21 +0300

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_list - frees a list_t list
+ * @head: head of the linked list
+ *
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *nodes;
+
+	while((nodes = head) != NULL)
+	{
+		head = head->next;
+		free(nodes->str);
+		free(nodes);
+	}
+}

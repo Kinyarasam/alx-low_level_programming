611e11139ebe52b70ed2f388f57846b93e705295
kinyarasam <samkinyara2000@gmail.com>
2022-04-26 20:26:42 +0300

    Betty style

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -5,14 +5,14 @@
 /**
  * free_listint - frees a listint_t list
  * @head: head of the linked list
- * 
+ *
  * Return: nothing
  */
 void free_listint(listint_t *head)
 {
 	listint_t *current;
 
-	while((current = head) != NULL)
+	while ((current = head) != NULL)
 	{
 		head = head->next;
 		free(current);

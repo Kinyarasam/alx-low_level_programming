deb9f23a8fd978e1d4ec9ba2272f27dab727c782
kinyarasam <samkinyara2000@gmail.com>
2022-04-26 23:49:40 +0300

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -29,7 +29,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 				nodes = nodeAt;
 				nodeAt = nodeAt->next;
 				nodes->next = nodeAt->next;
-				free (nodeAt);
+				free(nodeAt);
 				return (1);
 			}
 			nodeAt = nodeAt->next;

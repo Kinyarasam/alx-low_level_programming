b7da8219e89613e41a17ed526146a5424ce8fa9e
kinyarasam <samkinyara2000@gmail.com>
2022-04-29 00:46:23 +0300

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,10 +10,11 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 63)
+	if (index < 63)
 	{
 		*n = *n | (1 << index);
 		return (1);
+	}else{
+		return (-1);
 	}
-	return (-1);
 }

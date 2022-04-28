11830fad97e1a59b04ddaed1836e08eac65c2b51
kinyarasam <samkinyara2000@gmail.com>
2022-04-29 00:52:10 +0300

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * clear-bit - sets the value to 0 at a given index
+ * clear_bit - sets the value to 0 at a given index
  * @n: pointer of an unsigned long int
  * @index: index of the bit
  *
  * Return: Always 1.(Success)
  * otherwise: -1.
  */
-int clear_bit(unsigned long int*n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);

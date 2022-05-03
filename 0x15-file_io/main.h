d8170705de6e4da47755425eac4faac2c6a64e25
kinyarasam <samkinyara2000@gmail.com>
2022-05-03 14:24:20 +0300

    main.h

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif

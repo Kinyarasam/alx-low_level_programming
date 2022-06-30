9775a6d790058e5410b56a9edfbb8d54740c66a7
kinyarasam <samkinyara2000@gmail.com>
2022-06-30 13:22:37 +0300

    adds an element to the hash table.

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -7,7 +7,7 @@
 #include <string.h>
 
 /**
- * struc hash_node_s - Node of a hash table
+ * struct hash_node_s - Node of a hash table
  *
  * @key: The key, string
  * The key is unique in the HashTable
@@ -38,5 +38,6 @@ typedef struct hash_table_s
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 
 #endif /* _HASH_TABLES_H_ */
